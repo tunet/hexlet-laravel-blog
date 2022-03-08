0270e6b4ee94274f039373f1495fee6d7e7bf107
Aliaksandr Kulba <tunet000@gmail.com>
2022-03-08 19:28:48 +0300

    feat: add ArticleController with pagination

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,7 +9,7 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::paginate(1);
+        $articles = Article::paginate();
 
         return view('article.index', compact('articles'));
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
+    <div>
+        <a href="{{ route('articles.index') }}">Статьи</a>
+    </div>
     <div class="container mt-4">
         <h1>@yield('header')</h1>
         <div>
