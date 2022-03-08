8de69f373c6720aa8448eb146005bc63b2f11e82
Aliaksandr Kulba <tunet000@gmail.com>
2022-03-08 20:42:04 +0300

    feat: add article show action

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
-use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
@@ -12,5 +11,12 @@ class ArticleController extends Controller
         $articles = Article::paginate();
 
         return view('article.index', compact('articles'));
+    }
+
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+
+        return view('article.show', compact('article'));
     }
 }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,8 +3,14 @@
 @section('content')
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <h2>{{ $article->name }}</h2>
-        <div>{{ Str::limit($article->body, 200) }}</div>
+        <h2>
+            <a href="{{ route('articles.show', ['id' => $article->id]) }}">
+                {{ $article->name }}
+            </a>
+        </h2>
+        <div>
+            {{ Str::limit($article->body, 200) }}
+        </div>
     @endforeach
 @endsection
 
